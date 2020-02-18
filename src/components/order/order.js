f8d101d857365351ec48cfa96e38f2f79bc5df86
Ilya Trenin <itrenin@gmail.com>
2020-02-18 18:32:55 +0300

    некоторые исправленя 2

--- src/components/order/order.js
+++ src/components/order/order.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import Map from './map'
 
 import { useDispatch, useSelector } from 'react-redux'
 import { makeStyles } from '@material-ui/core/styles'
